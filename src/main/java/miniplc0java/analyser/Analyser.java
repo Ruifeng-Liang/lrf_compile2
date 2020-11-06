ff57ec9c118ccb2d18e25dcc5b13416a3084848d
no-dream <lrf123@buaa.edu.cn>
2020-11-06 16:30:07 +0800

    第sanci次提交

--- src/main/java/miniplc0java/analyser/Analyser.java
+++ src/main/java/miniplc0java/analyser/Analyser.java
@@ -460,6 +460,5 @@ public final class Analyser {
         if (negate) {
             instructions.add(new Instruction(Operation.SUB));
         }
-        throw new Error("Not implemented");
     }
 }
